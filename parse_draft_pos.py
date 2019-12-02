2ef20f7a82386cb75fdb155fdc09b529be8932f2
Frank Li <lilifan98@gmail.com>
2019-12-02 16:32:37 -0600

    added draft positions and groups

--- parse_draft_pos.py
+++ parse_draft_pos.py
@@ -1,7 +1,20 @@
 import pandas as pd
 
 df = pd.read_csv("data/nba_draft_1997_2018.csv")
+data = pd.read_csv("data/AllRookieDataWithDraftPos.csv")
+data['year'].fillna(data.season_id, inplace=True)
+data['round'].fillna(3, inplace=True)
+data['pick'].fillna(61, inplace=True)
+data['intl_game'].fillna(0, inplace=True)
+data['intl_minutes'].fillna(0, inplace=True)
+data['intl_group'].fillna(0, inplace=True)
+data['cbb_games'].fillna(0, inplace=True)
+data['cbb_minutes'].fillna(0, inplace=True)
+data['cbb_group'].fillna(0, inplace=True)
 
-df['First'] = df['First'].replace({'\'': '', '\\.': ''}, regex=True)
+data.to_csv("data/AllRookieData4.0.csv", index=False)
+# df['First'] = df['First'].replace({'\'': '', '\\.': ''}, regex=True)
 
-df.to_csv("data/nba_draft_1997_2018_parsed.csv", index=False)
+# df.to_csv("data/nba_draft_1997_2018_parsed.csv", index=False)
+
+
